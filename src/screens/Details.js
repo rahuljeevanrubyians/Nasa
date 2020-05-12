d2ce13ab707c61f484e53d3cacb7b889e90587f7
Rahul <rahul.jeevan@rubyians.com>
2020-05-12 18:34:49 +0530

    Details page added

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -3,6 +3,7 @@ import {
   //   KeyboardAvoidingView,
   SafeAreaView,
   Text,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import {connect} from 'react-redux';
@@ -13,25 +14,120 @@ class Details extends Component {
     this.state = {};
   }
   render() {
+    const {data} = this.props.fetch;
+
+    //     name
+    // - nasa_jpl_url
+    // - is_potentially_hazardous_asteroid
     return (
       <SafeAreaView
         style={{
           flex: 1,
           backgroundColor: '#fff',
         }}>
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.goBack();
+          }}
+          style={{
+            position: 'absolute',
+            left: 20,
+            top: 20,
+          }}>
+          <Text
+            style={{
+              fontSize: 80,
+            }}>
+            ↩︎
+          </Text>
+        </TouchableOpacity>
         <View
           style={{
             flex: 1,
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-          <Text
+          <View
             style={{
-              color: '#fff',
-              fontSize: 20,
+              borderWidth: 1,
+              width: '80%',
+              borderRadius: 15,
+              justifyContent: 'center',
+              paddingLeft: 10,
+              borderColor: '#03A9F4',
+              padding: 10,
             }}>
-            Submit
-          </Text>
+            <Text
+              style={{
+                fontSize: 18,
+                color: 'grey',
+              }}>
+              Name
+            </Text>
+            <Text
+              style={{
+                fontSize: 20,
+                color: '#03A9F4',
+              }}>
+              {data.name}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              borderWidth: 1,
+              width: '80%',
+              borderRadius: 15,
+              justifyContent: 'center',
+              paddingLeft: 10,
+              borderColor: '#03A9F4',
+              marginTop: 25,
+              padding: 10,
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+                color: 'grey',
+              }}>
+              Jet Propulsion Laboratory URL
+            </Text>
+            <Text
+              style={{
+                fontSize: 20,
+                color: '#03A9F4',
+              }}>
+              {data.nasa_jpl_url}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              borderWidth: 1,
+              width: '80%',
+              borderRadius: 15,
+              justifyContent: 'center',
+              paddingLeft: 10,
+              borderColor: '#03A9F4',
+              marginTop: 25,
+              padding: 10,
+            }}>
+            <Text
+              style={{
+                fontSize: 18,
+                color: 'grey',
+              }}>
+              Is Potentiallu Hazardous Asteroid?
+            </Text>
+            <Text
+              style={{
+                fontSize: 20,
+                color: '#03A9F4',
+              }}>
+              {data.is_potentially_hazardous_asteroid
+                ? data.is_potentially_hazardous_asteroid
+                : false}
+            </Text>
+          </View>
 
           {/* </ScrollView>
           </KeyboardAvoidingView> */}
